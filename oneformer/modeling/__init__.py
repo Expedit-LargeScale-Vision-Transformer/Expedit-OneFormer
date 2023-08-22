5f829305cf323a06f8a33c44e4eb672010ed6528
kxqt <liangweicong@stu.pku.edu.cn>
2023-08-22 08:10:13 +0000

    support hourglass

--- oneformer/modeling/__init__.py
+++ oneformer/modeling/__init__.py
@@ -1,4 +1,5 @@
 from .backbone.swin import D2SwinTransformer
+from .backbone.hourglass_swin import D2HourglassSwinTransformer
 from .backbone.dinat import D2DiNAT
 from .backbone.convnext import D2ConvNeXt
 from .pixel_decoder.fpn import BasePixelDecoder
